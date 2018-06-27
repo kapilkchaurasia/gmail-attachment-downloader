6b823b7367498ff0cabaa67ef2c66b99f2e8ff33
kapil chaurasia <kapil.chaurasia@grabtaxi.com>
2018-06-27 11:15:59 +0800

    attachment downloader

---
+++ fetchLiveUpEntries.py
@@ -0,0 +1,90 @@
+"""
+Shows basic usage of the Gmail API.
+
+Lists the user's Gmail labels.
+"""
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+from apiclient import errors
+import base64
+import sys
+
+def GetAttachments(service, user_id, msg_id, store_dir):
+    """Get and store attachment from Message with given id.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      msg_id: ID of Message containing attachment.
+      store_dir: The directory used to store attachments.
+    """
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+
+        for part in message['payload']['parts']:
+            if part['filename']:
+                if 'data' in part['body']:
+                    data=part['body']['data']
+                else:
+                    att_id=part['body']['attachmentId']
+                    att=service.users().messages().attachments().get(userId=user_id, messageId=msg_id,id=att_id).execute()
+                    data=att['data']
+                file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
+                path = store_dir+part['filename']
+
+                with open(path, 'a') as f:
+                    f.write(file_data)
+    except errors.HttpError, error:
+        print('An error occurred: %s' % error)
+
+def ListMessagesMatchingQuery(service, user_id, query=''):
+    """List all Messages of the user's mailbox matching the query.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      query: String used to filter messages returned.
+      Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+    Returns:
+      List of Messages that match the criteria of the query. Note that the
+      returned list contains Message IDs, you must use get with the
+      appropriate ID to get the details of a Message.
+    """
+    try:
+        response = service.users().messages().list(userId=user_id,
+                                                   q=query).execute()
+        messages = []
+        if 'messages' in response:
+            messages.extend(response['messages'])
+
+        while 'nextPageToken' in response:
+            page_token = response['nextPageToken']
+            response = service.users().messages().list(userId=user_id, q=query,
+                                                       pageToken=page_token).execute()
+            messages.extend(response['messages'])
+
+        return messages
+    except errors.HttpError, error:
+        print('An error occurred: %s' % error)
+
+
+# Setup the Gmail API
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+store = file.Storage('credentials.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets(sys.argv[2], SCOPES) #'client_secret.json'
+    creds = tools.run_flow(flow, store)
+service = build('gmail', 'v1', http=creds.authorize(Http()))
+listOfMessage = ListMessagesMatchingQuery(service, "me", "subject:daily-grab")
+
+for msg in listOfMessage:
+    print(msg['id'])
+    GetAttachments(service, "me", msg['id'], sys.argv[1] ) #"/Users/kapil.chaurasia/grabproject/pyemail"
+
+# Call the Gmail API
